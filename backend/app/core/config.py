d83b2f9be513d2398ae0b0ad062df5213b97143d
Korista89 <korista89@gmail.com>
2026-01-31 13:51:55 +0900

    Update Google Sheet URL

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Intelligent Behavior Support Dashboard"
     GOOGLE_CREDENTIALS_FILE: str = "service_account.json"
-    SHEET_URL: str = "" 
+    SHEET_URL: str = "https://docs.google.com/spreadsheets/d/1pMQIowYYBIk-6owcJqCNK5mA8GtssEEr6XdUq8gC9Cs/edit" 
     
     class Config:
         env_file = ".env"
